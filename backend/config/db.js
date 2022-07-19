ca28ee0801f45d4daf1ed7666084a0dedf9143fe
Sonish <sonishn213@gmail.com>
2022-07-19 23:56:52 +0530

    added some arguments

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
     console.log("Mobgo connected" + conn.connection.host);
   } catch (err) {
     console.log(err);
